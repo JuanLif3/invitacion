7e97dc8dcc88d9d7dc17d56f364cc438e5b9b922
JuanLif3 <65509709+JuanLif3@users.noreply.github.com>
2024-05-21 21:52:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
+const validUsers = [
+  { usuario: "vicente", contraseña: "password1", pagina: "vicente.html" },
+  { usuario: "camilo", contraseña: "password2", pagina: "camilo.html" },
+  { usuario: "cristobal", contraseña: "password3", pagina: "cristobal.html" },
+  { usuario: "jose", contraseña: "password4", pagina: "jose.html" },
+  { usuario: "CyJ", contraseña: "password5", pagina: "CyJ.html" },
+  { usuario: "JyV", contraseña: "password6", pagina: "JyV.html" },
+  { usuario: "VyV", contraseña: "password7", pagina: "VyV.html" },
+  { usuario: "moises", contraseña: "password8", pagina: "moises.html" },
+  { usuario: "karla", contraseña: "password9", pagina: "karla.html" },
+  { usuario: "anais", contraseña: "password10", pagina: "anais.html" },
+  { usuario: "marc", contraseña: "password10", pagina: "marc.html" },
+  { usuario: "pedro", contraseña: "password11", pagina: "pedro.html" }
+];
 
-      const validUsers = [
-        {
-          usuario: "usuario1",
-          contraseña: "password1",
-          pagina: "bienvenido_usuario1.html",
-        },
-        {
-          usuario: "usuario2",
-          contraseña: "password2",
-          pagina: "bienvenido_usuario2.html",
-        },
-      ];
-
-      document
-        .getElementById("loginForm")
-        .addEventListener("submit", function (event) {
-          event.preventDefault();
-          const usuario = document.getElementById("usuario").value;
-          const contraseña = document.getElementById("contraseña").value;
-          const user = validUsers.find(
-            (user) => user.usuario === usuario && user.contraseña === contraseña
-          );
-
-          if (user) {
-            window.location.href = user.pagina;
-          } else {
-            document.getElementById("error").textContent =
-              "Usuario o contraseña incorrecta";
-          }
-        });
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const usuario = document.getElementById('usuario').value;
+  const contraseña = document.getElementById('contraseña').value;
+  const user = validUsers.find(user => user.usuario === usuario && user.contraseña === contraseña);
+  
+  if (user) {
+    window.location.href = user.pagina;
+  } else {
+    document.getElementById('error').textContent = "Usuario o contraseña incorrecta";
+  }
+});
